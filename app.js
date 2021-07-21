b5b8362996fdc9fe41f6714853f01cf07c49abc3
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-07-21 15:39:28 -0500

    Sequalize added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+// Importación de módulos;
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+// Asignación de módulos;
 const app = express();
-const port = process.env.PORT || 3000;
+const sequelize = new Sequelize("sqlite::memory");
+/* Construcción de módelo de datos para MySQL; */
+class peon extends Model {}
+
+//
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/* get y post*/
+
 app.get("/", function (req, res) {
-  res.send("HOLA");
+  res.send("funciona");
 });
 
+/* Levantar el serverrr */
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("server up at: " + port);
 });
