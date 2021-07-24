ee5f636a2edf1f47c04d96eaca566dff692e3744
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-07-23 20:02:15 -0500

    kind of functional

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 // Importación de módulos;
 const express = require("express");
-const bodyParser = require("body-parser");
+const multer = require("multer");
+const cors = require("cors");
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
 // Asignación de módulos;
 const app = express();
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpg", "image/jpeg", "image/png"];
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error("Incorrect file extension");
+    error.code = "INCORRECT_FILETYPE";
+    return cb(error, false);
+  }
+  cb(null, true);
+};
+const upload = multer({
+  dest: "./images",
+  fileFilter,
+});
+
+app.use((err, req, res, next) => {
+  if (err.code == "INCORRECT_FILETYPE") {
+    res.status(422).json({ error: "Sólo se aceptan imágenes" });
+    return;
+  }
+});
 
 // configuración de la base de datos;
 const sequelize = new Sequelize("alfil", "root", "", {
@@ -24,6 +46,7 @@ sequelize
     console.log("error: " + error);
   });
 */
+
 // Construcción de modelo de datos para MySQL;
 
 class Image extends Model {}
@@ -37,9 +60,13 @@ Image.init(
   { sequelize, modelName: "image", tableName: "images" }
 );
 
-// Image.sync({ force: true });
-
 // métodos para las llamadas CRUD
+
+function showAll() {
+  Image.findAll({
+    attributes: ["id"],
+  });
+}
 
 function createImage(imageId, imageName, imageComment, imageUrl) {
   Image.create({
@@ -54,9 +81,9 @@ function deleteImage(imageId) {
   Image.destroy({ where: { id: imageId } });
 }
 
-function updateImage(imageId, newName, newComentario) {
+function updateImage(imageId, newName, newComentario, newUrl) {
   Image.update(
-    { name: newName, comentario: newComentario },
+    { name: newName, comentario: newComentario, url: newUrl },
     {
       where: {
         id: imageId,
@@ -65,15 +92,39 @@ function updateImage(imageId, newName, newComentario) {
   );
 }
 
-//updateImage(1, "Coneja", "Hola");
+//Image.sync({ force: true });
+//updateImage(2, "hedionda", "a vainilla", "https://esverdad.com");
+//createImage(1, "daniel", "fullstack", "./images/hydra.jpg");
+//deleteImage(3);
+//showAll();
 
 /* enrutadores */
 
 app.get("/", function (req, res) {
-  res.send("funciona");
+  res.send("<h1>Funciona</h1>");
 });
 
-app.post("/", function (req, res) {});
+app.get("/delete", function (req, res) {});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.json({ file: req.file });
+  console.log(res.json());
+
+  /*let imageFile = req.files.file;
+  imageFile.mv(`./images/${imageFile.name}`, (err) => {
+    if (err) return res.status(500).send({ message: err });
+    return res.status(200).send({ message: "File upload" });
+  });*/
+});
+
+app.post("/update", function (req, res) {
+  res.send("");
+});
+app.post("/", function (req, res) {
+  /* req.body.data;
+  req.body.effect(data);
+*/
+});
 
 /* Levantar el serverrr */
 
